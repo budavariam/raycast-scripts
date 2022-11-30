efe83c53b04518e5a92dd1443db7a565be7f73eb
Mátyás Budavári <budavariam@gmail.com>
2022-11-30 23:16:56 +0100

    Add templates from repo

--- templates/script-command.template.rb
+++ templates/script-command.template.rb
@@ -1,16 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/env ruby
 
 # Raycast Script Command Template
 #
-# Dependency: This script requires Python 3
-# Install Python 3: https://www.python.org/downloads/release
+# Dependency: This script requires Ruby
+# Install Ruby: http://www.ruby-lang.org/
 #
 # Duplicate this file and remove ".template." from the filename to get started.
 # See full documentation here: https://github.com/raycast/script-commands
 #
 # Required parameters:
 # @raycast.schemaVersion 1
-# @raycast.title My First Python Script
+# @raycast.title My First Script
 # @raycast.mode fullOutput
 # @raycast.packageName Raycast Scripts
 #
@@ -18,10 +18,14 @@
 # @raycast.icon 🤖
 # @raycast.currentDirectoryPath ~
 # @raycast.needsConfirmation false
+# @raycast.argument1 { "type": "text", "placeholder": "Placeholder text" }
 #
 # Documentation:
 # @raycast.description Write a nice and descriptive summary about your script command here 
 # @raycast.author Your name
 # @raycast.authorURL An URL for one of your social medias
 
-print("Hello World!")
+# If accepting an argument:
+# arg1 = ARGV[0]
+
+puts "Hello World!"
