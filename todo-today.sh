2bbddb5a16a84c0cdef26d1b6aed05bb7c56614b
Mátyás Budavári <budavariam@gmail.com>
2022-12-04 17:36:11 +0100

    Open relevant todo

---
+++ todo-today.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# Raycast Script Command
+#
+# See full documentation here: https://github.com/raycast/script-commands
+#
+# Required parameters:
+# @raycast.schemaVersion 1
+# @raycast.title Open Today's TODO
+# @raycast.mode fullOutput
+#
+# Optional parameters:
+# @raycast.icon 📔
+# @raycast.packageName Raycast Scripts
+
+pushd "${HOME}/project/todolog/" || exit 1
+bash "today.sh"
+popd || exit 1
